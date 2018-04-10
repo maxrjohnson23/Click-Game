e59cd5102ac6de84a9ac349ffde6341ebead97cb
Max <lfmax223@gmail.com>
2018-04-10 14:30:32 -0500

    Add shuffle method for game tiles

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Card from "../Card";
 import "./Gameboard.css"
 import data from "./data"
 
+class GameBoard extends Component {
+    state = {
+        gameTiles: data
+    };
 
-const GameBoard = () => (
-    <div className="container">
-        <div className="gameboard">
-            {
-                data.map(card => <Card key={card.id} image={card.image}
-                                       name={card.name}/>)
-            }
-        </div>
-    </div>
+    onClickShuffleTiles = () => {
+        // Shuffle array
+        let shuffledArray = this.state.gameTiles;
 
-)
+        console.log('Shuffling');
+        for (let i = shuffledArray.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+        }
+        this.setState({
+            gameTiles: shuffledArray
+        });
+    };
+
+
+    render() {
+        return (
+            <div className="container">
+                <div className="gameboard" onClick={this.onClickShuffleTiles}>
+                    {
+                        this.state.gameTiles.map(card => <Card key={card.id}
+                                                               image={card.image}
+                                                               name={card.name}/>)
+                    }
+                </div>
+            </div>
+
+        )
+    };
+}
 
 export default GameBoard;
