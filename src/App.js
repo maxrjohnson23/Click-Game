4c6f74311242025929fe80bdc18276951c02642e
Max Johnson <maxrjohnson23@gmail.com>
2018-04-10 14:12:49 -0500

    Add navbar for scoring

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
 import GameBoard from './components/GameBoard';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <header className="App-header">
             <h1 className="App-title">Click Game</h1>
         </header>
-          <GameBoard/>
+        <GameBoard />
       </div>
     );
   }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Navbar = () => (
+    <nav>test</nav>
+);
+
+export default Navbar;
