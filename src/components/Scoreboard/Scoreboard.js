0ca87fa28cda3c1b4e20652fb77209897a550b30
Max <lfmax223@gmail.com>
2018-04-16 11:22:13 -0500

    Update animation css

--- src/components/Scoreboard/Scoreboard.js
+++ src/components/Scoreboard/Scoreboard.js
@@ -7,7 +7,7 @@ const Scoreboard = (props) => (
         your score, but don't click the same tile twice!</h4>
       <div class="scores">
         <h2 className={props.animate ? "shake" : ""}>Score: {props.currentScore}</h2>
-        <h2> | </h2><h2 id="highScore"><i
+        <h2> | </h2><h2 className="highScore"><i
           className="fa fa-trophy"/> High score: {props.highScore}</h2>
       </div>
     </div>
